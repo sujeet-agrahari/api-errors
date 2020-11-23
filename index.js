3b99b49f3aada12378311c9f7286bb4195b71972
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2020-11-23 16:49:16 +0530

    setup-package

---
+++ index.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-multi-assign */
+/* eslint-disable no-param-reassign */
+/* eslint-disable max-classes-per-file */
+class APIError extends Error {
+  constructor(status, message) {
+    super();
+    this.status = status;
+    this.message = message;
+  }
+}
+
+const apiErrors = Object.entries({
+  BadRequest: {
+    statusCode: 400,
+    message: 'Bad Request',
+  },
+  Unauthorized: {
+    statusCode: 401,
+    message: 'Unauthorized',
+  },
+  AccessDeniedError: {
+    statusCode: 401,
+    message: 'Access denied',
+  },
+  Forbidden: {
+    statusCode: 403,
+    message: 'Forbidden',
+  },
+  NotFound: {
+    statusCode: 404,
+    message: 'Not found',
+  },
+  Conflict: {
+    statusCode: 409,
+    message: 'Conflict',
+  },
+  UnSupportedMediaType: {
+    statusCode: 415,
+    message: 'Unsupported Media Type',
+  },
+  UnProcessableEntity: {
+    statusCode: 422,
+    message: 'Unprocessable Entity',
+  },
+  InternalServer: {
+    statusCode: 500,
+    message: 'Internal Server Error',
+  },
+  MethodNotAllowed: {
+    statusCode: 405,
+    message: 'Method Not Allowed',
+  },
+}).reduce((map, [name, data]) => {
+  map[`${name}Error`] = map[name] = class extends APIError {
+    constructor(message = data.message) {
+      super(data.statusCode, message);
+    }
+  };
+  return map;
+}, {});
+
+module.exports = {
+  ...apiErrors,
+  APIError,
+};
